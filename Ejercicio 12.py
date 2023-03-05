187b5845f6a374bc043f48f3a3ffba76915ae335
Alejandro Castro <111087081+AlejandroC10P@users.noreply.github.com>
2023-03-04 19:16:16 -0500

    Ejercicio 12

---
+++ Ejercicio 12.py
@@ -0,0 +1,4 @@
+SalarioBruto = 48*5000
+Retencion = SalarioBruto*0.125
+SalarioNeto = SalarioBruto - Retencion
+print(f'Información del trabajador:\nSalario bruto: {SalarioBruto}\nRetención en la fuente: {Retencion}\nSalario neto: {SalarioNeto}')
